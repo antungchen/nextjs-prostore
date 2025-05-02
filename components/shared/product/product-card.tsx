18354fb0bd689105a1b2bc1ff72a06105e84b48c
Jiyuan Chen <chenantung@gmail.com>
2025-05-02 22:03:42 +1000

    feat: Zod Validation & Type Inference

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,7 +2,9 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import ProductPrice from "./product-price";
-const ProductCard = ({ product }: { product: any }) => {
+import { Product } from "@/types";
+
+const ProductCard = ({ product }: { product: Product }) => {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader>
