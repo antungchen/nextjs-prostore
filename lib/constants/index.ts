8f28dd5cc215321ddbcc6c43dcbb09da184193d7
Jiyuan Chen <chenantung@gmail.com>
2025-05-05 21:30:05 +1000

    4.41: Sign Up & Form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -11,3 +11,10 @@ export const signInDefaultValues = {
   email: "admin@example.com",
   password: "123456",
 };
+
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
