6841f2228ca56442830ec42f5d36da912da295c2
Jiyuan Chen <chenantung@gmail.com>
2025-05-05 22:13:12 +1000

    4.43: minor update

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,8 +8,10 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: "admin@example.com",
-  password: "123456",
+  // email: "admin@example.com",
+  // password: "123456",
+  email: "email@example.com",
+  password: "password",
 };
 
 export const signUpDefaultValues = {
