fa50753c6b44a9760144d041497bb8b4acdfb97b
Jiyuan Chen <chenantung@gmail.com>
2025-05-05 21:58:16 +1000

    4.43: Customize Token With JWT Callback

--- auth.ts
+++ auth.ts
@@ -57,15 +57,36 @@ export const config = {
   ],
   callbacks: {
     async session({ session, user, trigger, token }: any) {
-      //set user id from token
+      // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
-      //If there is a update, set the user name
+      // If there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // If user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update the database to reflect the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
