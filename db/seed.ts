2cb5a0bc133202be3a32aa4b588474f16523827b
Jiyuan Chen <chenantung@gmail.com>
2025-05-05 15:05:23 +1000

    4.31: Seed User Data

--- db/seed.ts
+++ db/seed.ts
@@ -5,8 +5,18 @@ async function main() {
   const prisma = new PrismaClient();
   // Delete all existing products
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  // Create new products
+
   // Create new products
   await prisma.product.createMany({ data: sampleData.products });
+
+  // Create new users
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
