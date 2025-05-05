065450064be00fe8e49b31cdbd2bc35615fd2197
Jiyuan Chen <chenantung@gmail.com>
2025-05-05 13:57:13 +1000

    feat: Product Details Page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -16,3 +16,10 @@ export async function getLatestProducts() {
   //     rating: product.rating.toString(),
   //   }));
 }
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
