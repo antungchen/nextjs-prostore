c091fa79aa9931744d27cb8a78a989b8d95cbe37
Jiyuan Chen <chenantung@gmail.com>
2025-05-08 16:21:08 +1000

    7.69: Order Zod Schemas & Types

--- lib/validators.ts
+++ lib/validators.ts
@@ -85,3 +85,26 @@ export const paymentMethodSchema = z
     path: ["type"],
     message: "Invalid payment method",
   });
+
+// Schema for inserting order
+export const insertOrderSchema = z.object({
+  userId: z.string().min(1, "User id is required"),
+  itemsPrice: currency,
+  taxPrice: currency,
+  shippingPrice: currency,
+  totalPrice: currency,
+  paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+    message: "Invalid payment method",
+  }),
+  shippingAddress: shippingAddressSchema,
+});
+
+// Schema for inserting order item
+export const insertOrderItemSchema = z.object({
+  productId: z.string().min(1, "Product id is required"),
+  name: z.string().min(1, "Name is required"),
+  slug: z.string().min(1, "Slug is required"),
+  image: z.string().min(1, "Image is required"),
+  price: currency,
+  quantity: z.number().int().nonnegative("Quantity must be a positive number"),
+});
